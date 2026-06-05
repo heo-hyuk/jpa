94325ae5e2a42a541ff0759effce9e1c59e446c4
heo-hyuk <exgjgur4@gmail.com>
2026-06-05 10:18:50 +0900

    ok

---
+++ src/main/java/com/example/jpa/entity/Post.java
@@ -0,0 +1,14 @@
+package com.example.jpa.entity;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+    private  LocalDateTime updatedAt;
+
+
+}
